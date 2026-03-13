b7cd8e6f3797968ca225b0ae8be06b2314f1fa12
Kaique Santos <kaiquesantos.dev@gmail.com>
2026-03-13 17:30:31 -0300

    Changes by lucas

--- src/main/java/br/com/ufcg/rsa/math/PrimeGenerator.java
+++ src/main/java/br/com/ufcg/rsa/math/PrimeGenerator.java
@@ -1,5 +1,11 @@
 package br.com.ufcg.rsa.math;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class PrimeGenerator {
 
+    public static BigInteger generatePrime(int bitLength) {
+        return BigInteger.probablePrime(bitLength, new SecureRandom());
+    }
 }
--- src/main/java/br/com/ufcg/rsa/model/PublicKey.java
+++ src/main/java/br/com/ufcg/rsa/model/PublicKey.java
@@ -1,5 +1,24 @@
 package br.com.ufcg.rsa.model;
 
-public class PublicKey {
+import java.math.BigInteger;
 
+public class PublicKey {
+    private final BigInteger n;
+    private static final BigInteger LAMBDA = new BigInteger("65537");
+
+    public PublicKey(BigInteger n) {
+        this.n = n;
+    }
+
+    public BigInteger getN() {
+        return n;
+    }
+
+    public BigInteger getLambda() {
+        return LAMBDA;
+    }
+
+    public static BigInteger calculateN(BigInteger p, BigInteger q) {
+        return p.multiply(q);
+    }
 }
